1a947155c170c31bbbc7322cf0a99b706783253c
josejavier <82359964+josejaviersanahuja@users.noreply.github.com>
2021-08-07 19:26:30 +0100

    responsiveness ok. podemos continuar

--- api-monitoring-app/public/app.js
+++ api-monitoring-app/public/app.js
@@ -5,4 +5,9 @@
 
 const app = {}
 
+const handleMenuBtnClick = () => {
+    const menu = document.getElementsByClassName('menu')[0]
+    menu.classList.toggle('open')
+}
+
 console.log('hello console front');
